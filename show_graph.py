cbf645ad41aeb08452301d55725415530174b994
Daniel Košč <daniel.kosc@gmail.com>
2025-01-30 14:27:37 +0100

    Fixing plot from asymmetric data (different measurement for different QoS)

--- show_graph.py
+++ show_graph.py
@@ -72,17 +72,17 @@ def load_data(directory) -> list:
 
 average_agg_files(data_folder)
 
-qos = [name for name in os.listdir(data_folder) if os.path.isdir(os.path.join(data_folder, name))]
+qos = [int(name) for name in os.listdir(data_folder) if os.path.isdir(os.path.join(data_folder, name))]
 dataset = dict()
-subscribers = []
+subscribers = [[] for _ in range(len(qos))]
 for selected_qos in qos:
-    subscribers = [name for name in os.listdir(os.path.join(data_folder, selected_qos)) if
-                   os.path.isdir(os.path.join(data_folder, selected_qos, name))]
+    subscribers[selected_qos] = [name for name in os.listdir(os.path.join(data_folder, str(selected_qos))) if
+                   os.path.isdir(os.path.join(data_folder, str(selected_qos), name))]
     dataset[selected_qos] = dict()
-    for subscriber in subscribers:
-        dataset[selected_qos][subscriber] = load_data(os.path.join(data_folder, selected_qos, subscriber))
+    for subscriber in subscribers[selected_qos]:
+        dataset[selected_qos][subscriber] = load_data(os.path.join(data_folder, str(selected_qos), subscriber))
 
-subscribers = list(map(str, sorted(list(map(int, subscribers)))))
+    subscribers[selected_qos] = list(map(str, sorted(list(map(int, subscribers[selected_qos])))))
 
 # Extracting data for plotting
 frequency = dict()
@@ -90,10 +90,10 @@ bandwidth = dict()
 for selected_qos in qos:
     frequency[selected_qos] = dict()
     bandwidth[selected_qos] = dict()
-    for subscriber in subscribers:
+    for subscriber in subscribers[selected_qos]:
         frequency[selected_qos][subscriber] = [entry[3] for entry in dataset[selected_qos][subscriber]]
         bandwidth[selected_qos][subscriber] = [entry[2] / 1e6 for entry in dataset[selected_qos][subscriber]]
-sizes = [entry[1] for entry in dataset[qos[0]][subscribers[0]]]
+sizes = [entry[1] for entry in dataset[qos[0]][subscribers[selected_qos][0]]]
 
 # Plotting
 fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10, 12))
@@ -101,10 +101,10 @@ fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10, 12))
 # Frequency plot
 ax1.set_title("Frequency and Bandwidth vs Size")
 for selected_qos in qos:
-    for subscriber in subscribers:
+    for subscriber in subscribers[selected_qos]:
         mark = 'o-' if selected_qos == '0' else 'v-'
         ax1.loglog(sizes, frequency[selected_qos][subscriber], mark,
-                   label=format_subscriber(subscriber) + " - Subscribers; QoS - " + selected_qos)
+                   label=format_subscriber(subscriber) + " - Subscribers; QoS - " + str(selected_qos))
 
 if expected_payload > 0:
     ax1.axvline(x=expected_payload * 1024, color='red', linestyle='--')
@@ -121,10 +121,10 @@ ax1.set_xticks([])  # Remove x-axis labels for top plot
 # Bandwidth plot
 
 for selected_qos in qos:
-    for subscriber in subscribers:
+    for subscriber in subscribers[selected_qos]:
         mark = 'o-' if selected_qos == '0' else 'v-'
         ax2.loglog(sizes, bandwidth[selected_qos][subscriber], mark,
-                   label=format_subscriber(subscriber) + " - Subscriber; QoS - " + selected_qos)
+                   label=format_subscriber(subscriber) + " - Subscriber; QoS - " + str(selected_qos))
 if expected_payload > 0:
     ax2.axvline(x=expected_payload * 1024, color='red', linestyle='--')
     ax2.annotate('requirement', xy=(expected_payload * 1024, ax2.get_ylim()[0]),
